016d4f09b9b52417e07728d32b8be9430af6438a
Joost van Zwam <joost.vanzwam@altran.com>
2020-09-11 11:02:09 +0200

    Fix deployment using acidcli

--- releng/scripts/build.sh
+++ releng/scripts/build.sh
@@ -1,31 +1,8 @@
 #!/usr/bin/env bash
 
-targetBranch="$1"
-tag="$2"
 updateSite="develop"
-profiles="acceptance-test"
 
-if [ -n "$2" ]; then
-	profiles+=",publish"
-	baselineTag=$2
-	dots=${baselineTag//[^.]}
-	if [ ${#dots} == 2 ]; then
-		updateSite=${baselineTag%.*}
-	else
-		updateSite=${baselineTag}
-	fi
-else if [ "$targetBranch" == "develop" ]; then
-	profiles+=",publish"
-fi fi
-
-if [[ $profiles == *"publish"* ]]; then
-	if curl --output /dev/null --silent --head --fail --user $ArtifactStoreUserName:$ArtifactStorePassword "https://nexus.acidspace.nl/repository/MDEAssets/espilce-periksa/$updateSite/content.jar"; then
-	    profiles+=",merge-existing-repository"
-fi fi
-
-echo Building branch $targetBranch with profiles $profiles and update site $updateSite
-
-mvn clean install --fail-at-end -P$profiles \
+mvn clean install --fail-at-end \
 	-f releng/org.espilce.periksa.parent/pom.xml \
 	-s releng/settings.xml \
 	-Dmde.reactor.maven.debug=false \
@@ -36,5 +13,3 @@ mvn clean install --fail-at-end -P$profiles \
 build_success=$?
 
 exit $build_success
-
-
